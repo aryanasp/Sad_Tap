3703e78ec86c81bd05b13c8e55a1d066b7e0a493
aryanasp <aryanasp76@gmail.com>
2021-01-08 18:44:40 +0330

    Design start menu, Implement game manager and score

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //state
+    int score = 0;
+
+    public int Score { get => score; set => score = value; }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        int numberGameSeesions = FindObjectsOfType(GetType()).Length;
+        if (numberGameSeesions > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    public void AddToScore(int scoreValue)
+    {
+        Score += scoreValue;
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    //config
+    [SerializeField]
+    private float delayForSeconds = 2f;
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene("Start Screen");
+    }
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game Screen");
+        FindObjectOfType<GameManager>().ResetGame();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+}
